3c44ac96542ebb6a48ee134fac7789d80ff57199
Hunter Stephens <game.guy7000@gmail.com>
2020-03-06 09:28:39 -0700

    Inclusion of First Time Setup Window

---
+++ DataWrangler/FirstTimeSetup.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DataWrangler
+{
+    partial class FirstTimeSetup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NextButoon = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.FilePathBox = new System.Windows.Forms.TextBox();
+            this.FileBrowseButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NextButoon
+            // 
+            this.NextButoon.Enabled = false;
+            this.NextButoon.Location = new System.Drawing.Point(413, 286);
+            this.NextButoon.Name = "NextButoon";
+            this.NextButoon.Size = new System.Drawing.Size(207, 45);
+            this.NextButoon.TabIndex = 3;
+            this.NextButoon.Text = "Next";
+            this.NextButoon.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(148, 208);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(17, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(174, 24);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Create new database";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Book Antiqua", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(17, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(251, 24);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Connect to an existing database";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // FilePathBox
+            // 
+            this.FilePathBox.Location = new System.Drawing.Point(196, 208);
+            this.FilePathBox.Name = "FilePathBox";
+            this.FilePathBox.ReadOnly = true;
+            this.FilePathBox.Size = new System.Drawing.Size(379, 20);
+            this.FilePathBox.TabIndex = 7;
+            // 
+            // FileBrowseButton
+            // 
+            this.FileBrowseButton.Location = new System.Drawing.Point(581, 208);
+            this.FileBrowseButton.Name = "FileBrowseButton";
+            this.FileBrowseButton.Size = new System.Drawing.Size(27, 21);
+            this.FileBrowseButton.TabIndex = 8;
+            this.FileBrowseButton.Text = "...";
+            this.FileBrowseButton.UseVisualStyleBackColor = true;
+            this.FileBrowseButton.Click += new System.EventHandler(this.FileBrowseButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(152, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(498, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " ";
+            // 
+            // FirstTimeSetup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.FileBrowseButton);
+            this.Controls.Add(this.FilePathBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NextButoon);
+            this.Name = "FirstTimeSetup";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button NextButoon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox FilePathBox;
+        private System.Windows.Forms.Button FileBrowseButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ DataWrangler/FirstTimeSetup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataWrangler
+{
+    public partial class FirstTimeSetup : Form
+    {
+        public FirstTimeSetup()
+        {
+            InitializeComponent();
+            var configSettings = ConfigurationHelper.GetDbSettings();
+            configSettings.TryGetValue("dbFilePath", out var dbPath);
+            if (dbPath != null)
+            {
+                var login = new Login(configSettings);
+                login.Show();
+                this.Close();
+            
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void FileBrowseButton_Click(object sender, EventArgs e)
+        {
+
+            var filePath = string.Empty;
+            if (radioButton1.Checked)
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    
+                    openFileDialog.Filter = "Database Files|*.db"; //Filter not working properly due to desktop location
+                    openFileDialog.FilterIndex = 2;
+                    openFileDialog.RestoreDirectory = true;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        FilePathBox.Text = openFileDialog.FileName;
+                        NextButoon.Enabled = true;
+
+
+                        ConfigurationHelper.SaveDbSettings(FilePathBox.Text);
+                    }
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                    saveFileDialog.Filter = "database files (*.db)|*.db"; //Filter not working properly
+                    saveFileDialog.FilterIndex = 1;
+                    saveFileDialog.RestoreDirectory = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        //Get the path of specified file
+                        FilePathBox.Text = saveFileDialog.FileName;
+                        NextButoon.Enabled = true;
+                        ConfigurationHelper.SaveDbSettings(FilePathBox.Text);
+                    }
+                }
+            }
+            //MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+    }
+}
