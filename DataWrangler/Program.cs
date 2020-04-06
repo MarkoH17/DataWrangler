ea04d73abdec25b4427a3f98f3166c22b2fc65b6
Mark Hedrick <MarkoH17@users.noreply.github.com>
2020-04-06 15:18:21 -0600

    Update Solution Build Config

--- DataWrangler/Program.cs
+++ DataWrangler/Program.cs
@@ -11,6 +11,7 @@ namespace DataWrangler
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
+        ///
         [STAThread]
         private static void Main()
         {
